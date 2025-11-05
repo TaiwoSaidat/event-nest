393ee9e371367ab55878e8a14d304eda9988f8cf
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2025-11-05 12:35:36 +0100

    project set up

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className=" bg-red-800-500 text-3xl font-bold underline border-4 border-amber-800 ">
+        EVENT NEST
+      </div>
+    </>
+  );
+}
