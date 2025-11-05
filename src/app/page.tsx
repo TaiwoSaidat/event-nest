6056dac3cfe70bc8114f55afc65d975fa335f2f0
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2025-11-05 13:45:45 +0100

    chore: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className=" bg-red-800-500 text-3xl font-bold underline border-4 border-amber-800 ">
-        EVENT NEST
+      <div className=" bg-red-800 text-3xl font-bold underline border-4 border-amber-800 ">
+        EVENssT NEST
       </div>
     </>
   );
